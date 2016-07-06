32d0686f00091063ceb8184ee1b4dce88ad6b72b
wang4yu6peng13 <wang4yu6peng13@qq.com>
2016-07-06 15:01:54 +0800

    update Ch09 GUI

---
+++ Ch09/treeExplore.py
@@ -0,0 +1,74 @@
+#coding:utf-8
+from numpy import *
+from Tkinter import *
+import regTrees
+
+# Matplotlib和Tkinter的代码集成
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+def reDraw(tolS, tolN):
+    reDraw.f.clf()
+    reDraw.a = reDraw.f.add_subplot(111)
+    if chkBtnVar.get(): # 检查复选框是否选中
+        if tolN < 2:
+            tolN = 2
+        myTree = regTrees.createTree(reDraw.rawDat, regTrees.modelLeaf, regTrees.modelErr, (tolS, tolN))
+        yHat = regTrees.createForeCast(myTree, reDraw.testDat, regTrees.modelTreeEval)
+    else:
+        myTree = regTrees.createTree(reDraw.rawDat, ops=(tolS, tolN))
+        yHat = regTrees.createForeCast(myTree, reDraw.testDat)
+    reDraw.a.scatter(reDraw.rawDat[:,0], reDraw.rawDat[:,1], s=5)
+    reDraw.a.plot(reDraw.testDat, yHat, linewidth=2.0)
+    reDraw.canvas.show()
+
+def getInputs():
+    try:
+        tolN = int(tolNentry.get())
+    except:
+        tolN = 10
+        print "enter Integer for tolN"
+        # 清除错误的输入并用默认值替换
+        tolNentry.delete(0, END)
+        tolNentry.insert(0, '10')
+    try:
+        tolS = float(tolSentry.get())
+    except:
+        tolS = 1.0
+        print "enter Float for tolS"
+        tolSentry.delete(0, END)
+        tolSentry.insert(0, '1.0')
+    return tolN, tolS
+
+def drawNewTree():
+    tolN, tolS = getInputs()
+    reDraw(tolS, tolN)
+
+# 用于构建树管理器界面的Tkinter小部件
+root = Tk()
+
+reDraw.f = Figure(figsize=(5,4), dpi=100)
+reDraw.canvas = FigureCanvasTkAgg(reDraw.f, master=root)
+reDraw.canvas.show()
+reDraw.canvas.get_tk_widget().grid(row=0, columnspan=3)
+
+Label(root, text="tolN").grid(row=1, column=0)
+tolNentry = Entry(root)
+tolNentry.grid(row=1, column=1)
+tolNentry.insert(0, '10')
+Label(root, text="tolS").grid(row=2, column=0)
+tolSentry = Entry(root)
+tolSentry.grid(row=2, column=1)
+tolSentry.insert(0, '1.0')
+Button(root, text="ReDraw", command=drawNewTree).grid(row=1, column=2,rowspan=3)
+chkBtnVar = IntVar()
+chkBtn = Checkbutton(root, text="Model Tree", variable=chkBtnVar)
+chkBtn.grid(row=3, column=0, columnspan=2)
+
+reDraw.rawDat = mat(regTrees.loadDataSet('sine.txt'))
+reDraw.testDat = arange(min(reDraw.rawDat[:,0]), max(reDraw.rawDat[:,0]), 0.01)
+reDraw(1.0, 10)
+
+root.mainloop()
